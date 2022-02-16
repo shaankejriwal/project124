5ab93226717d53825dc7c4f17a2a11ca83aa4c8e
shaankejriwal <70429914+shaankejriwal@users.noreply.github.com>
2022-02-16 13:23:16 +0530

    Add files via upload

---
+++ flaskapp.py
@@ -0,0 +1,50 @@
+from asyncio import Task
+from flask import Flask,jsonify,request
+app = Flask(__name__)
+
+tasks = [
+    {
+        "Conatct" : "9987644456",
+        "Name" : "Raju",
+        "done" : False,
+        "id" : 1
+    },
+    {
+        "Conatct" : "9876543222",
+        "Name" : "Rahul",
+        "done" : False,
+        "id" : 2
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return("Hello World")
+
+@app.route("/add-data",methods = ["POST"])
+def add_task():
+    if not request.json:
+        return jsonify({
+            "status" : "error",
+            "message" : "please provide the data",
+        },400)
+    task = {
+        "id" : tasks[-1]["id"]+1,
+        "Name" : request.json["Name"],
+        "Contact" : request.json.get("Contact",""),
+        "done" : False
+    }
+    tasks.append(task)
+    return jsonify({
+        "status" : "success",
+        "message" : "task added successfully"
+    })
+
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : tasks
+    })
+
+if __name__ == "__main__":
+    app.run(debug = True)
